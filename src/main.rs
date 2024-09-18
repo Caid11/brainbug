beb15be4e391a194066a42fd57b3cc7d1f3714f1
Kincaid Savoie <kincaidsavoie@gmail.com>
2024-09-18 17:45:51 -0600

    add profiling data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, VecDeque}, env, fs, io::{self, Read}, process::ExitCode, usize};
+use std::{collections::{HashMap, VecDeque}, env, fmt, fs, io::{self, Read}, process::ExitCode, time::SystemTime, usize};
 
 enum Instruction {
     MoveRight,
@@ -11,12 +11,29 @@ enum Instruction {
     JumpUnlessZero
 }
 
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Instruction::MoveRight => write!(f, ">"),
+            Instruction::MoveLeft => write!(f, "<"),
+            Instruction::Increment => write!(f, "+"),
+            Instruction::Decrement => write!(f, "-"),
+            Instruction::Write => write!(f, "+"),
+            Instruction::Read => write!(f, "-"),
+            Instruction::JumpIfZero => write!(f, "["),
+            Instruction::JumpUnlessZero => write!(f, "]"),
+        }
+    }
+}
+
 struct State {
     tape: VecDeque<u8>,
     head_pos: usize,
 
     program_counter: usize,
     program: Vec<Instruction>,
+    execution_counter: Vec<usize>,
+
     jump_dests: HashMap<usize, usize>,
 }
 
@@ -98,12 +115,37 @@ fn compute_jump_dests(insts : &Vec<Instruction>) -> HashMap<usize, usize> {
     return jump_dests;
 }
 
+#[derive(Eq)]
+struct LoopExecution {
+    pc : usize,
+    num_times_executed : usize,
+}
+
+impl Ord for LoopExecution {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.num_times_executed.cmp(&self.num_times_executed)
+    }
+}
+
+impl PartialOrd for LoopExecution {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for LoopExecution {
+    fn eq(&self, other: &Self) -> bool {
+        self.num_times_executed == other.num_times_executed
+    }
+}
+
 impl State {
     pub fn new(input: &str) -> Self {
         let mut t = VecDeque::new();
         t.push_back(0);
 
         let program = lex(input);
+        let execution_counter = vec![0; program.len()];
         let jump_dests = compute_jump_dests(&program);
 
         State {
@@ -111,6 +153,7 @@ impl State {
             head_pos: 0,
             program_counter: 0,
             program,
+            execution_counter,
             jump_dests,
         }
     }
@@ -192,6 +235,8 @@ impl State {
                 break;
             }
 
+            self.execution_counter[self.program_counter] += 1;
+
             match self.program[self.program_counter] {
                 Instruction::MoveRight => self.move_right(),
                 Instruction::MoveLeft => self.move_left(),
@@ -204,22 +249,150 @@ impl State {
             }
         }
     }
+
+    fn get_loop_executions(&self) -> (Vec<LoopExecution>, Vec<LoopExecution>) {
+        let mut simple_loops : Vec<LoopExecution> = Vec::new();
+        let mut complex_loops : Vec<LoopExecution> = Vec::new();
+
+        let mut curr_loop : Option<LoopExecution> = Option::None;
+        let mut has_io = false;
+        let mut pointer_offset : i32 = 0;
+        let mut pointer_value : i32 = 0;
+
+        for pc in 0..self.program.len() {
+            match self.program[pc] {
+                Instruction::MoveRight => pointer_offset += 1,
+                Instruction::MoveLeft => pointer_offset -= 1,
+                Instruction::Write | Instruction::Read => has_io = true,
+
+                Instruction::Increment => {
+                    if pointer_offset == 0 {
+                        pointer_value += 1;
+                    }
+                },
+                Instruction::Decrement => {
+                    if pointer_offset == 0 {
+                        pointer_value -= 1;
+                    }
+                },
+
+                _ => (),
+            }
+
+            match self.program[pc] {
+                Instruction::JumpIfZero => {
+                    curr_loop = Some(LoopExecution{pc, num_times_executed: 0});
+                    has_io = false;
+                    pointer_offset = 0;
+                    pointer_value = 0;
+                },
+
+                Instruction::JumpUnlessZero => {
+                    let curr = curr_loop;
+                    curr_loop = Option::None;
+
+                    let index_changed_by_1 = pointer_value.abs() == 1;
+
+                    match curr {
+                        Option::None => (),
+                        Option::Some(l) => {
+                            if !has_io && pointer_offset == 0 && index_changed_by_1 {
+                                simple_loops.push(l);
+                            } else {
+                                complex_loops.push(l);
+                            }
+                        }
+                    }
+                },
+
+                _ => {
+                    match curr_loop {
+                        Option::None => (),
+                        Option::Some(ref mut l) => {
+                            if l.num_times_executed == 0 {
+                                l.num_times_executed = self.execution_counter[pc];
+                            }
+                        },
+                    }
+                },
+            }
+        }
+
+        simple_loops.sort();
+        complex_loops.sort();
+
+        return (simple_loops, complex_loops);
+    }
+
+    fn print_profile_info(&mut self)
+    {
+        println!("PC\tOP\t# EXECUTED");
+        for pc in 0..self.program.len() {
+            println!("{}\t{}\t{}", pc, self.program[pc], self.execution_counter[pc]);
+        }
+
+        let (simple_loops, complex_loops) = self.get_loop_executions();
+
+        println!("\nSIMPLE LOOPS");
+        println!("PC\t# EXECUTED");
+        for l in simple_loops {
+            println!("{}\t{}", l.pc, l.num_times_executed);
+        }
+
+        println!("\nCOMPLEX LOOPS");
+        println!("PC\t# EXECUTED");
+        for l in complex_loops {
+            println!("{}\t{}", l.pc, l.num_times_executed);
+        }
+    }
 }
 
 fn print_usage() {
-    println!("Usage: brainbug [path to bf file]");
+    println!("Usage: brainbug [path to bf file] [options]");
+    println!("Options: -p  Print profile data");
+    println!("         -t  Print execution time");
 }
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        print_usage();
-        return ExitCode::from(1);
+
+    let mut file_path = "";
+    let mut profile = false;
+    let mut time = false;
+
+    for i in 1..args.len() {
+        // Flag arguments
+        if args[i] == "-p" {
+            profile = true;
+            continue;
+        } else if args[i] == "-t" {
+            time = true;
+            continue;
+        }
+
+        // Positional arguments
+        if !file_path.is_empty() {
+            print_usage();
+            return ExitCode::from(1);
+        }
+
+        file_path = &args[i];
     }
 
-    let program = fs::read_to_string(args[1].clone()).expect("unable to read file");
+    let program = fs::read_to_string(file_path).expect("unable to read file");
+
+    let start_time = SystemTime::now();
+
     let mut state = State::new(&program);
     state.interp();
+
+    if time {
+        println!("\nExecution time: {}", start_time.elapsed().unwrap().as_secs_f64());
+    }
+
+    if profile {
+        state.print_profile_info();
+    }
 
     return ExitCode::from(0);
 }
@@ -344,4 +517,178 @@ mod tests {
         assert_eq!(state.tape[2], 50);
     }
 
+    #[test]
+    fn test_execution_counter() {
+        let program = "+++++[>+<-]";
+
+        let mut state = State::new(&program);
+        state.interp();
+
+        assert_eq!(state.execution_counter[0], 1);
+        assert_eq!(state.execution_counter[1], 1);
+        assert_eq!(state.execution_counter[2], 1);
+        assert_eq!(state.execution_counter[3], 1);
+        assert_eq!(state.execution_counter[4], 1);
+
+        assert_eq!(state.execution_counter[5], 5);
+        assert_eq!(state.execution_counter[6], 5);
+        assert_eq!(state.execution_counter[7], 5);
+        assert_eq!(state.execution_counter[8], 5);
+        assert_eq!(state.execution_counter[9], 5);
+    }
+
+    #[test]
+    fn test_get_loop_profile_no_loops() {
+        let program = "+++++";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 0);
+        assert_eq!(complex_loops.len(), 0);
+    }
+
+    #[test]
+    fn test_get_loop_profile_one_simple() {
+        let program = ">+++[>+++<-]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 1);
+        assert_eq!(simple_loops[0].pc, 4);
+        assert_eq!(simple_loops[0].num_times_executed, 3);
+
+        assert_eq!(complex_loops.len(), 0);
+    }
+
+    #[test]
+    fn test_get_loop_profile_one_simple_no_exe() {
+        let program = "+++>[>+++<-]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 1);
+        assert_eq!(simple_loops[0].pc, 4);
+        assert_eq!(simple_loops[0].num_times_executed, 0);
+
+        assert_eq!(complex_loops.len(), 0);
+    }
+
+    #[test]
+    fn test_get_loop_profile_one_complex_io() {
+        let program = ">+++[>.+++<-]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 0);
+
+        assert_eq!(complex_loops.len(), 1);
+        assert_eq!(complex_loops[0].pc, 4);
+        assert_eq!(complex_loops[0].num_times_executed, 3);
+    }
+
+    #[test]
+    fn test_get_loop_profile_one_complex_pointer_offset_change() {
+        let program = ">+++[>]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 0);
+
+        assert_eq!(complex_loops.len(), 1);
+        assert_eq!(complex_loops[0].pc, 4);
+        assert_eq!(complex_loops[0].num_times_executed, 1);
+    }
+
+    #[test]
+    fn test_get_loop_profile_one_complex_index() {
+        let program = ">++++[>+<--]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 0);
+
+        assert_eq!(complex_loops.len(), 1);
+        assert_eq!(complex_loops[0].pc, 5);
+        assert_eq!(complex_loops[0].num_times_executed, 2);
+    }
+
+    #[test]
+    fn test_get_loop_profile_simple_nested() {
+        let program = ">+++[>+++++[>++<-]<-]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 1);
+        assert_eq!(simple_loops[0].pc, 11);
+        assert_eq!(simple_loops[0].num_times_executed, 15);
+
+        assert_eq!(complex_loops.len(), 0);
+    }
+
+    #[test]
+    fn test_get_loop_profile_complex_nested() {
+        let program = ">+++[>++++++[>++<--]<-]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 0);
+
+        assert_eq!(complex_loops.len(), 1);
+        assert_eq!(complex_loops[0].pc, 12);
+        assert_eq!(complex_loops[0].num_times_executed, 9);
+    }
+
+    #[test]
+    fn test_get_loop_profile_simple_sorted() {
+        let program = "+++[>--<-]++[>--<-]++++[>--<-]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 3);
+        assert_eq!(simple_loops[0].pc, 23);
+        assert_eq!(simple_loops[0].num_times_executed, 4);
+        assert_eq!(simple_loops[1].pc, 3);
+        assert_eq!(simple_loops[1].num_times_executed, 3);
+        assert_eq!(simple_loops[2].pc, 12);
+        assert_eq!(simple_loops[2].num_times_executed, 2);
+
+        assert_eq!(complex_loops.len(), 0);
+    }
+
+    #[test]
+    fn test_get_loop_profile_complex_sorted() {
+        let program = "++++[>--<--]++[>--<--]++++++[>--<--]";
+
+        let mut state = State::new(&program);
+        state.interp();
+        
+        let (simple_loops, complex_loops) = state.get_loop_executions();
+        assert_eq!(simple_loops.len(), 0);
+
+        assert_eq!(complex_loops.len(), 3);
+        assert_eq!(complex_loops[0].pc, 28);
+        assert_eq!(complex_loops[0].num_times_executed, 3);
+        assert_eq!(complex_loops[1].pc, 4);
+        assert_eq!(complex_loops[1].num_times_executed, 2);
+        assert_eq!(complex_loops[2].pc, 14);
+        assert_eq!(complex_loops[2].num_times_executed, 1);
+    }
 }
